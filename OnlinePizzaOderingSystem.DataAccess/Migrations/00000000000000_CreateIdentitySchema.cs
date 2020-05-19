5f96beaa34fec30ff43c30d7170f4d76c4e3e3ad
ThaymesCoder <thamue1892@gmail.com>
2020-05-19 02:36:54 +0200

    Added Areas to application

--- OnlinePizzaOderingSystem.DataAccess/Migrations/00000000000000_CreateIdentitySchema.cs
+++ OnlinePizzaOderingSystem.DataAccess/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace OnlinePizzaOrderingSystem.Data.Migrations
+namespace OnlinePizzaOrderingSystem.DataAccess.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
